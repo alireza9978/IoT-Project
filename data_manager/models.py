dcee919d2508200a152716c93beecd482e3433d4
mohammad esmaeely <esmaeely1377@gmail.com>
2022-05-08 15:29:39 +0430

    data manager: sensor data model + migration file

--- data_manager/models.py
+++ data_manager/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from gateway.models import Sensor
+
+
+class SensorData(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, verbose_name=_("sensor"))
+    temperature = models.IntegerField(verbose_name=_("temperature"))
+    humidity = models.IntegerField(verbose_name=_("humidity"))
+    brightness = models.IntegerField(verbose_name=_("brightness"))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("created at"))
+
+    class Meta:
+        verbose_name = _("sensor data")
+        verbose_name_plural = _("sensor data")
+
+    def __str__(self):
+        return f'{self.sensor} - {self.created_at}'
