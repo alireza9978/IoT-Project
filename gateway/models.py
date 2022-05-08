8673ad5b9cb89cd29b3313e9f6fbbceff286858e
mohammad esmaeely <esmaeely1377@gmail.com>
2022-05-08 11:47:09 +0430

    gateway: gateway and sensor model + migration file

--- gateway/models.py
+++ gateway/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Gateway(models.Model):
+    mac_address = models.CharField(unique=True, max_length=150, verbose_name=_("mac address"))
+
+    class Meta:
+        verbose_name = _("gateway")
+        verbose_name_plural = _("gateways")
+
+    def __str__(self):
+        return self.mac_address
+
+
+class Sensor(models.Model):
+    gateway = models.ForeignKey(Gateway, on_delete=models.CASCADE, verbose_name=_("gateway"), related_name="sensors")
+
+    class Meta:
+        verbose_name = _("sensor")
+        verbose_name_plural = _("sensors")
+
+    def __str__(self):
+        return f'{self.gateway} - {self.id}'
