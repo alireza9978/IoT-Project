948d27d90b33ee3a38dc4445c5335bd3ea54c823
mohammad esmaeely <esmaeely1377@gmail.com>
2022-05-09 16:53:57 +0430

    data manager: always return true for receiver data

--- backend/gateway/permissions.py
+++ backend/gateway/permissions.py
@@ -5,14 +5,11 @@ from gateway.models import Gateway
 
 class IsGateway(BasePermission):
     def has_permission(self, request, view):
-        # return bool(
-        #     hasattr(request.data, '')
-        # )
-        mac_address = request.META.get('HTTP_MAC', None)
-        if not mac_address:
-            return False
-        try:
-            gateway = Gateway.objects.get(mac_address=mac_address)
-        except Gateway.DoesNotExist:
-            return False
+        # mac_address = request.META.get('HTTP_MAC', None)
+        # if not mac_address:
+        #     return False
+        # try:
+        #     gateway = Gateway.objects.get(mac_address=mac_address)
+        # except Gateway.DoesNotExist:
+        #     return False
         return True
