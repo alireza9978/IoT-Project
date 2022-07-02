dd21388a415b9bd382561e09016e6ac395601d5b
mohammad esmaeely <esmaeely1377@gmail.com>
2022-07-03 02:23:06 +0430

    backend: sensor data view

--- backend/data_manager/urls.py
+++ backend/data_manager/urls.py
@@ -1,11 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
-from data_manager.views.sensor_views import SensorReceiveDataViewSet
+from data_manager.views.sensor_views import SensorReceiveDataViewSet, SensorDataView
 
 router = DefaultRouter()
 
 router.register('new_sensor_data', SensorReceiveDataViewSet, basename='new_sensor_data')
-
+router.register('sensor_data', SensorDataView, basename='sensor_data')
 
 urlpatterns = [
 ] + router.urls
--- backend/data_manager/views/sensor_views.py
+++ backend/data_manager/views/sensor_views.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import mixins, status, serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
 from data_manager.models import SensorData
 from gateway.models import Sensor
@@ -34,3 +34,14 @@ class SensorReceiveDataViewSet(mixins.CreateModelMixin, GenericViewSet):
         except Exception as e:
             return Response(data={"errors": e.args}, status=status.HTTP_400_BAD_REQUEST)
         return Response(status=status.HTTP_200_OK)
+
+
+class SensorDataView(ModelViewSet):
+    class SensorDataSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = SensorData
+            fields = '__all__'
+    queryset = SensorData.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = SensorDataSerializer
+    filterset_fields = ['sensor', 'sensor__user']
